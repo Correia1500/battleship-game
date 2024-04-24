eddc651682f4b0163d6e87bb58a4fa8adb659f12
Correia1500 <156720020+Correia1500@users.noreply.github.com>
2024-04-24 16:42:31 -0300

    bem-vindos

--- src/main.py
+++ src/main.py
@@ -1,66 +1,14 @@
-# Script principal que executa o jogo
+#BIBLIOTECAS
+import random
 
-# quantidade de blocos por modelo de navio
-CONFIGURACAO = {
-    'destroyer': 3,
-    'porta-avioes': 5,
-    'submarino': 2,
-    'torpedeiro': 3,
-    'cruzador': 2,
-    'couracado': 4
-}
-
-# frotas de cada pais
-PAISES =  {
-    'Brasil': {
-        'cruzador': 1,
-        'torpedeiro': 2,
-        'destroyer': 1,
-        'couracado': 1,
-        'porta-avioes': 1
-    }, 
-    'França': {
-        'cruzador': 3, 
-        'porta-avioes': 1, 
-        'destroyer': 1, 
-        'submarino': 1, 
-        'couracado': 1
-    },
-    'Austrália': {
-        'couracado': 1,
-        'cruzador': 3, 
-        'submarino': 1,
-        'porta-avioes': 1, 
-        'torpedeiro': 1
-    },
-    'Rússia': {
-        'cruzador': 1,
-        'porta-avioes': 1,
-        'couracado': 2,
-        'destroyer': 1,
-        'submarino': 1
-    },
-    'Japão': {
-        'torpedeiro': 2,
-        'cruzador': 1,
-        'destroyer': 2,
-        'couracado': 1,
-        'submarino': 1
-    }
-}
-
-# alfabeto para montar o nome das colunas
-ALFABETO = 'ABCDEFGHIJKLMNOPQRSTUVWXYZ'
-
-# cores para o terminal
-CORES = {
-    'reset': '\u001b[0m',
-    'red': '\u001b[31m',
-    'black': '\u001b[30m',
-    'green': '\u001b[32m',
-    'yellow': '\u001b[33m',
-    'blue': '\u001b[34m',
-    'magenta': '\u001b[35m',
-    'cyan': '\u001b[36m',
-    'white': '\u001b[37m'
-}
+#CORES
+cores = {'limpa': '\033[m',
+         'azul': '\033[34m',
+         'verde': '\033[32m',
+         'vermelho': '\033[31m',
+         'amarelo': '\033[33m',
+         'pretoebraco': '\033[7;30m',
+         }
+print(f"{cores['vermelho']} ==================================={cores['limpa']}")
+print(f"{cores['vermelho']}|{cores['limpa']} {cores['verde']}Bem-vindos a Batalha Naval INSPER{cores['limpa']} {cores['vermelho']}|{cores['limpa']}")
+print(f"{cores['vermelho']} ==================================={cores['limpa']}")
