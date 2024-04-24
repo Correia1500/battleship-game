b298373ed928b40993b729f31e0e3ca063d9eadb
Correia1500 <156720020+Correia1500@users.noreply.github.com>
2024-04-24 20:48:28 -0300

    introdução

--- src/main.py
+++ src/main.py
@@ -3,15 +3,10 @@ import random
 import threading
 from playsound import playsound #pip install playsound ##para baixar
 import time #efeito no terminal
-
+#funcoes
+from utils import *
 ##cores
-cores = {'limpa': '\033[m',
-    'azul': '\033[34m',
-    'verde': '\033[32m',
-       'vermelho': '\033[31m',
-         'amarelo': '\033[33m',
-         'pretoebraco': '\033[7;30m',
-         }
+from constantes import *
 time.sleep(1)
 # Função para tocar o som
 def musica_fundo():
@@ -23,19 +18,25 @@ music_thread.start()
 
 delay = 0.4
 
-print(f"{cores['vermelho']} ==================================={cores['limpa']}")
-print(f"{cores['vermelho']}|{cores['limpa']} {cores['verde']}Bem-vindos a Batalha Naval INSPER{cores['limpa']} {cores['vermelho']}|{cores['limpa']}")
-print(f"{cores['vermelho']} ==================================={cores['limpa']}")
-time.sleep(1)
+print(f"{CORES['red']} ===============xxx================={CORES['reset']}")
+print(f"{CORES['red']}|{CORES['reset']} {CORES['green']}Bem-vindos a Batalha Naval INSPER{CORES['reset']} {CORES['red']}|{CORES['reset']}")
+print(f"{CORES['red']} ===============xxx================={CORES['reset']}")
+time.sleep(delay)
+
+tempo = input('Quer pular a animação ?[S/N]')
+if tempo == "S":
+    delay = 0.1
+else:
+    delay = 2
 
 print("Iniciando o Jogo!")
-time.sleep(1)
+time.sleep(delay)
 
 print("Computador está alocando os navios de guerra do país Japão...")
-time.sleep(2)
+time.sleep(delay)
 
 print("Computador já está em posição de batalha!")
-time.sleep(2)
+time.sleep(delay)
 
 
 
@@ -45,33 +46,79 @@ print("                                        2 torpedeiro")
 print("                                        1 destroyer")
 print("                                        1 couracado")
 print("                                        1 porta-avioes")
-time.sleep(1)
+time.sleep(delay)
 print("2: França")
 print("                                        3 cruzador")
 print("                                        1 porta-avioes")
 print("                                        1 destroyer")
 print("                                        1 submarino")
 print("                                        1 couracado")
-print("                                        1 couracado")
-time.sleep(1)
+time.sleep(delay)
 print("3: Austrália")
+print("                                        1 couracado")
 print("                                        3 cruzador")
 print("                                        1 submarino")
 print("                                        1 porta-avioes")
 print("                                        1 torpedeiro")
-time.sleep(1)
+time.sleep(delay)
 print("4: Rússia")
 print("                                        1 cruzador")
 print("                                        1 porta-avioes")
 print("                                        2 couracado")
 print("                                        1 destroyer")
 print("                                        1 submarino")
-time.sleep(1)
+time.sleep(delay)
 print("5: Japão")
 print("                                        2 torpedeiro")
 print("                                        1 cruzador")
 print("                                        2 destroyer")
 print("                                        1 couracado")
 print("                                        1 submarino")
-time.sleep(1)
-print("Qual o número da nação da sua frota? |")
+time.sleep(delay)
+
+###########posiciona o computador###########
+
+m = 10 #tamanho do mapa
+
+#sorteia um pais para o comp
+
+pais_comp = random.choice(['Brasil', 'França', 'Austrália', 'Rússia', 'Japão'])
+
+#lista do tamanho das embarcações
+#x =
+cria_mapa(m) ##retorna matriz 10x10
+aloca_navios(m,x) #recebe uma matriz e uma lista, posicionando os navios.
+
+dic_pais = {1: 'Brasil', 2: 'França', 3: 'Austrália', 4: 'Rússia', 5: 'Japão'}
+############################################
+
+
+
+
+
+
+
+
+nacao = int(input("Qual o número da nação da sua frota? ")) 
+
+from constantes import paises
+
+dic_pais = {1: 'Brasil', 2: 'França', 3: 'Austrália', 4: 'Rússia', 5: 'Japão'}
+nacao = int(input("Qual o número da nação da sua frota? ")) 
+dic_pais[nacao]
+print(f'Você escolheu a nação {dic_pais[nacao]}')
+print("Aloque seus Navios e Prepare-se para a Guerra!")
+print(paises[dic_pais[nacao]])
+
+
+m = 10
+print(cria_mapa(m))
+
+aloca_navios(m,paises[dic_pais[nacao[0]]])
+
+
+
+
+
+
+music_thread.join()
