45b671aa14278a687067439bef8dc58b14b0a2ce
Correia1500 <156720020+Correia1500@users.noreply.github.com>
2024-04-24 17:21:41 -0300

    add music

--- src/main.py
+++ src/main.py
@@ -1,7 +1,9 @@
 #BIBLIOTECAS
 import random
+import threading
+from playsound import playsound #pip install playsound ##para baixar
 
-#CORES
+##cores
 cores = {'limpa': '\033[m',
          'azul': '\033[34m',
          'verde': '\033[32m',
@@ -9,6 +11,17 @@ cores = {'limpa': '\033[m',
          'amarelo': '\033[33m',
          'pretoebraco': '\033[7;30m',
          }
+
+# Função para tocar o som
+def musica_fundo():
+    playsound("docs/march-of-the-templars-globus-preliator.mp3")
+
+# Iniciar a thread de música
+music_thread = threading.Thread(target=musica_fundo)
+music_thread.start()
+
+
 print(f"{cores['vermelho']} ==================================={cores['limpa']}")
 print(f"{cores['vermelho']}|{cores['limpa']} {cores['verde']}Bem-vindos a Batalha Naval INSPER{cores['limpa']} {cores['vermelho']}|{cores['limpa']}")
 print(f"{cores['vermelho']} ==================================={cores['limpa']}")
+
