e109401f3e9a23d3b6f5fe910fb2314d5f6e6dc4
Mariana <marianass13@al.insper.edu.br>
2024-04-30 14:52:06 -0300

    finalizações

--- src/main.py
+++ src/main.py
@@ -12,142 +12,158 @@ time.sleep(1)
 def musica_fundo():
     playsound("docs/march-of-the-templars-globus-preliator.mp3")
 
-# Iniciar a thread de música
-music_thread = threading.Thread(target=musica_fundo)
-music_thread.start()
+jd='S'#gostaria de jogar novamente?
 
-delay = 0.4
+while jd == 'S' or jd == 's':
 
-print(f"{CORES['red']} ===============xxx================={CORES['reset']}")
-print(f"{CORES['red']}|{CORES['reset']} {CORES['green']}Bem-vindos a Batalha Naval INSPER{CORES['reset']} {CORES['red']}|{CORES['reset']}")
-print(f"{CORES['red']} ===============xxx================={CORES['reset']}")
-time.sleep(delay)
+    # Iniciar a thread de música
+    music_thread = threading.Thread(target=musica_fundo)
+    music_thread.start()
 
-temp = input('Quer pular a animação ?[S/N]')
-temp_maiusc = temp.upper()
-if temp_maiusc == "S":    
-    delay = 0.1
-else:
-    delay = 2
+    delay = 0.4
 
-print("Iniciando o Jogo!")
-time.sleep(delay)
+    mj=( cria_mapa (10)) #mapa do jogador
+    mc=( cria_mapa (10)) #mapa do computador
 
-pc= random.choice(['Brasil', 'França', 'Austrália', 'Rússia', 'Japão'])
+    print(f"{CORES['red']} ===============xxx================={CORES['reset']}")
+    print(f"{CORES['red']}|{CORES['reset']} {CORES['green']}Bem-vindos a Batalha Naval INSPER{CORES['reset']} {CORES['red']}|{CORES['reset']}")
+    print(f"{CORES['red']} ===============xxx================={CORES['reset']}")
+    time.sleep(delay)
 
-print(f"Computador está alocando os navios de guerra do país {pc}")
-time.sleep(delay)
+    temp = input('Quer pular a animação ?[S/N]')
+    temp_maiusc = temp.upper()
+    if temp_maiusc == "S":    
+        delay = 0.1
+    else:
+        delay = 2
 
-print("Computador já está em posição de batalha!")
-time.sleep(delay)
+    print("Iniciando o Jogo!")
+    time.sleep(delay)
 
-mj=( cria_mapa (10)) #mapa do jogador
-mc=( cria_mapa (10)) #mapa do computador
+    pc= random.choice(['Brasil', 'França', 'Austrália', 'Rússia', 'Japão'])
+
+    print(f"Computador está alocando os navios de guerra do país {pc}")
+    time.sleep(delay)
+
+    print("Computador já está em posição de batalha!")
+    time.sleep(delay)
+        
+    lbc = lis_blocos (PAISES, CONFIGURACAO, pc) #lista do numero de blocos do computador
+
+    mi = aloca_navios(mc,lbc) #mapa inicial computador
+
+    print("1: Brasil")
+    print("                                        1 cruzador")
+    print("                                        2 torpedeiro")
+    print("                                        1 destroyer")
+    print("                                        1 couracado")
+    print("                                        1 porta-avioes")
+    time.sleep(delay)
+    print("2: França")
+    print("                                        3 cruzador")
+    print("                                        1 porta-avioes")
+    print("                                        1 destroyer")
+    print("                                        1 submarino")
+    print("                                        1 couracado")
+    time.sleep(delay)
+    print("3: Austrália")
+    print("                                        1 couracado")
+    print("                                        3 cruzador")
+    print("                                        1 submarino")
+    print("                                        1 porta-avioes")
+    print("                                        1 torpedeiro")
+    time.sleep(delay)
+    print("4: Rússia")
+    print("                                        1 cruzador")
+    print("                                        1 porta-avioes")
+    print("                                        2 couracado")
+    print("                                        1 destroyer")
+    print("                                        1 submarino")
+    time.sleep(delay)
+    print("5: Japão")
+    print("                                        2 torpedeiro")
+    print("                                        1 cruzador")
+    print("                                        2 destroyer")
+    print("                                        1 couracado")
+    print("                                        1 submarino")
+    time.sleep(delay)
+
+
+
+    lbc = lis_blocos(PAISES, CONFIGURACAO, pc) #lista do numero de blocos do computador
+
+
+
+
+
+    pj= int(input('Qual o numero da nação da sua frota? ')) #pais do jogador
+
+    disc_paises = {1:'Brasil',2:'França',3:'Austrália',4:'Rússia',5:'Japão'}
+
+    while pj not in disc_paises:
+        print('nenhuma nação corresponde a este numero, escolher outro')
+        pj= int(input('Qual o numero da nação da sua frota? ')) #pais do jogador
+
+    fp=[] #frota do pais 
+    lbj=[] #lista numero de blocos do jogador
+
+
+    x=disc_paises[pj]
+
+    for a in PAISES[x]:
+        for b in range(PAISES[x][a]):
+            fp.append(a)
+            lbj.append(CONFIGURACAO[a])
+
+
+
+    al=input('Gostaria de aleatorizar sua frota?[s/n]')
+
+    if al == 'n' or al == 'N':
+        print(aloca_navios_j(mj, lbj, fp))
+
+
+
+    elif al == 's' or al == 'S':
+        mj= aloca_navios(mj, lbj)
+        texj = imprime_matriz(mj, 'Jogador') #mapa do jogador formatado
+
+        print(texj)
+
+
+
+    while foi_derrotado(mj)== False and foi_derrotado(mc)==False:
+        mj=disparo_computador(mj)
+        mc=disparo_jogador(mi)
+        
+        texj = imprime_matriz(mj, 'Jogador')
+
+        print(texj)
+
+        
+        texc = imprime_matriz_c(mc, 'Computador') #mapa do computador formatado
+
+        print(texc)  
+        i=0
+        j=0
+        foi_derrotado(mj)
+        i=0
+        j=0
+        foi_derrotado(mc)
+        if foi_derrotado(mj)== True:
+            print('Voce perdeu')
+            jd= input ('Gostaria de jogar novamente?[s/n]')
+            break
+        elif foi_derrotado(mc) == True:
+            print('Voce venceu')
+            print(imprime_matriz(mj, 'Jogador'))
+            print(imprime_matriz(mc, 'Computador'))
+            jd= input ('Gostaria de jogar novamente?[s/n]')
+            break
     
-lbc = lis_blocos (PAISES, CONFIGURACAO, pc) #lista do numero de blocos do computador
-
-mi = aloca_navios(mc,lbc) #mapa inicial computador
-
-print("1: Brasil")
-print("                                        1 cruzador")
-print("                                        2 torpedeiro")
-print("                                        1 destroyer")
-print("                                        1 couracado")
-print("                                        1 porta-avioes")
-time.sleep(delay)
-print("2: França")
-print("                                        3 cruzador")
-print("                                        1 porta-avioes")
-print("                                        1 destroyer")
-print("                                        1 submarino")
-print("                                        1 couracado")
-time.sleep(delay)
-print("3: Austrália")
-print("                                        1 couracado")
-print("                                        3 cruzador")
-print("                                        1 submarino")
-print("                                        1 porta-avioes")
-print("                                        1 torpedeiro")
-time.sleep(delay)
-print("4: Rússia")
-print("                                        1 cruzador")
-print("                                        1 porta-avioes")
-print("                                        2 couracado")
-print("                                        1 destroyer")
-print("                                        1 submarino")
-time.sleep(delay)
-print("5: Japão")
-print("                                        2 torpedeiro")
-print("                                        1 cruzador")
-print("                                        2 destroyer")
-print("                                        1 couracado")
-print("                                        1 submarino")
-time.sleep(delay)
-
-mj=( cria_mapa (10)) #mapa do jogador
-mc=( cria_mapa (10)) #mapa do computador
-
-lbc = lis_blocos(PAISES, CONFIGURACAO, pc) #lista do numero de blocos do computador
-
-mi = aloca_navios(mc,lbc) #mapa inicial computador
-
-pj= int(input('Qual o numero da nação da sua frota? ')) #pais do jogador
-
-fp=[] #frota do pais 
-lbj=[] #lista numero de blocos do jogador
-disc_paises = {1:'Brasil',2:'França',3:'Austrália',4:'Rússia',5:'Japão'}
-
-x=disc_paises[pj]
-
-for a in PAISES[x]:
-    for b in range(PAISES[x][a]):
-        fp.append(a)
-        lbj.append(CONFIGURACAO[a])
-
-
-al=input('Gostaria de aleatorizar sua frota?[s/n]')
-
-if al == 'n':
-    print(aloca_navios_j(mj, lbj, fp))
+    
 
 
 
-elif al == 's':
-    mj= aloca_navios(mj, lbj)
-    for i in mj:
-        print(i)
-
-tex = imprime_matriz(mc, 'Computador') and imprime_matriz(mj, 'Jogador')
-
-print(tex)
-
-foi_derrotado(mj)
-foi_derrotado(mi)
 
 
-while foi_derrotado(mj)== False and foi_derrotado(mc)==False:
-    mj=disparo_computador(mj)
-    mc=disparo_jogador(mi)
-    print('Jogador')
-    texj = imprime_matriz(mj, 'Jogador')
-
-    print(tex)
-
-    print('Computador')
-    texc = imprime_matriz(mc, 'Computador')
-
-    print(tex)  
-    foi_derrotado(mj)
-    foi_derrotado(mc)
-    if foi_derrotado(mj)== True:
-        print('Voce perdeu')
-        break
-    elif foi_derrotado(mc)==True:
-        print('Voce venceu')
-        break
-
-
-
-# Uso da função:
-imprime_matriz(mj, "COMPUTADOR - Brasil")
-imprime_matriz(mc, "JOGADOR - França")
